d6cd1534e84ef56e7115bd902e8eecbface10939
Vage <gamebred7.github.io>
2021-04-17 13:47:21 +0300

    Messages component

---
+++ src/components/Messages/Messages.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ScrollToBottom from "react-scroll-to-bottom";
+
+import "./Messages.css";
+
+const Messages = () => <ScrollToBottom></ScrollToBottom>;
+
+export default Messages;
