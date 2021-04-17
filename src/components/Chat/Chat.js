ed1bd619840c621051bc81d8d6c2032ce1f6e968
Vage <gamebred7.github.io>
2021-04-17 14:44:53 +0300

    Message component

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -4,6 +4,7 @@ import io from "socket.io-client";
 
 import InfoBar from "../InfoBar/InfoBar";
 import Input from "../Input/Input";
+import Messages from "../Messages/Messages";
 
 import "./Chat.css";
 
@@ -21,23 +22,21 @@ const Chat = ({ location }) => {
 
     socket = io(ENDPOINT);
 
-    setName(name);
     setRoom(room);
+    setName(name);
 
-    socket.emit("join", { name, room }, () => {});
-
-    return () => {
-      socket.emit("dicconnect");
-
-      socket.off();
-    };
+    socket.emit("join", { name, room }, (error) => {
+      if (error) {
+        alert(error);
+      }
+    });
   }, [ENDPOINT, location.search]);
 
   useEffect(() => {
     socket.on("message", (message) => {
-      setMessages(...messages, message);
+      setMessages((messages) => [...messages, message]);
     });
-  }, [messages]);
+  }, []);
 
   const sendMessage = (event) => {
     event.preventDefault();
@@ -53,6 +52,7 @@ const Chat = ({ location }) => {
     <div className="outerContainer">
       <div className="container">
         <InfoBar room={room} />
+        <Messages messages={messages} name={name} />
         <Input
           message={message}
           setMessage={setMessage}
