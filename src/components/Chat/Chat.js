4e78422f6ead94840b54c0b4da1d74fd378a3f6d
Vage <gamebred7.github.io>
2021-04-17 13:07:45 +0300

    InfoBar component

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -2,6 +2,11 @@ import React, { useState, useEffect } from "react";
 import queryString from "query-string";
 import io from "socket.io-client";
 
+import InfoBar from "../InfoBar/InfoBar";
+import Input from "../Input/Input";
+
+import "./Chat.css";
+
 let socket = io();
 
 const Chat = ({ location }) => {
@@ -47,11 +52,7 @@ const Chat = ({ location }) => {
   return (
     <div className="outerContainer">
       <div className="container">
-        <input
-          value={message}
-          onChange={(event) => setMessage(event.target.value)}
-          onKeyPress={(event) => (event.key == "Enter" ? sendMessage() : null)}
-        />
+        <InfoBar room={room} />
       </div>
     </div>
   );
