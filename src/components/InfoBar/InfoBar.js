0ddf7149ddd3a5171a9b6431e8b46f0013eec5b9
Vage <gamebred7.github.io>
2021-04-19 21:47:01 +0300

    Small fixes

--- src/components/InfoBar/InfoBar.js
+++ src/components/InfoBar/InfoBar.js
@@ -6,13 +6,11 @@ import onlineIcon from "../../icons/onlineIcon.png";
 const InfoBar = ({ room }) => (
   <div className="infoBar">
     <div className="leftInnerContainer">
-      <img className="onlineIcon" src={onlineIcon} alt="online" />
+      {/* <img className="onlineIcon" src={onlineIcon} alt="online" /> */}
       <h3>{room}</h3>
     </div>
     <div className="rightInnerContainer">
-      <a href="/">
-        <img src={closeIcon} alt="close" />
-      </a>
+      <a href="/">{/* <img src={closeIcon} alt="close" /> */}</a>
     </div>
   </div>
 );
